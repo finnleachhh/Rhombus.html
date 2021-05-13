94627612f1fabbbfe537f33b691918c523610919
Finn Leach <82237132+finnleachhh@users.noreply.github.com>
2021-05-12 21:19:02 -0700

    Update Rhombus.js

--- Rhombus.js
+++ Rhombus.js
@@ -4,9 +4,38 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
+}
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=1;i<=pHeight;i++){
+rLine +="<p>";
+
+for (x=1; x<=pHeight-i;x++){
+	//rLine +="-";
+	rLine +="<span class='space'" + pColorEven + ";'>" + pSymbol +"</span>";
+}
+//Create each line on the Rhombus
+for(j=x;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upLeft").innerHTML = rLine;
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -33,7 +62,7 @@ rLine +="</p>";
 document.getElementById("upRight").innerHTML = rLine;
 }
 
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=pHeight;i > 0;i--){
 rLine +="<p>";
@@ -55,4 +84,30 @@ rLine +="</p>";
 }
 
 document.getElementById("downRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=1;j<=i;j++){
+
+//rLine +="-";
+	rLine +="<span class='space'" + pColorEven + ";'>" + pSymbol +"</span>";
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downLeft").innerHTML = rLine;
 }
